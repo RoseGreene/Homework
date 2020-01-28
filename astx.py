f775f12099f9a632d1b0226b47dfdc68df5a651b
Varditer <53918941+RoseGreene@users.noreply.github.com>
2020-01-29 02:26:28 +0400

    patker

---
+++ astx.py
@@ -0,0 +1,14 @@
+num = 5
+
+for i in range(num):
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+for i in range(num, 0, -1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+    
+
+
