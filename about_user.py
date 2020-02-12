0bf16bd96853af7a0fbe1d90fc4ca71416c78c22
Varditer <53918941+RoseGreene@users.noreply.github.com>
2020-02-13 01:30:12 +0400

    dict with user inputs

---
+++ about_user.py
@@ -0,0 +1,11 @@
+my_dict = {}
+name = input("Enter you name: ")
+age = int(input("Enter your age: "))
+city = input("Where do you live? ")
+month = input("Enter the month you were born: ")
+my_dict["name"]=name
+my_dict["age"]=age
+my_dict["city"]=city
+my_dict["month"]=month
+
+print(my_dict)
