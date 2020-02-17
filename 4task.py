b6c3e6dd636e73c10d3487eb61e87238c8ebadf8
Varditer <53918941+RoseGreene@users.noreply.github.com>
2020-02-17 21:06:24 +0400

    exam

---
+++ 4task.py
@@ -0,0 +1,11 @@
+user_input = input("write a word: ")
+class Word:
+    def get_String(self):
+        user_input
+    def print_String(self):
+        print(user_input.upper())
+
+
+printed = Word()
+printed.get_String()
+printed.print_String()
